dff3d085c2fa2d2f7164e242a2ec28bb84e8ccf8
Ruslan S. <fehax@icloud.com>
2024-04-01 17:00:37 +0300

    implemented conditional rendering based on player status

--- src/components/PlayerControls/PlayerControls.tsx
+++ src/components/PlayerControls/PlayerControls.tsx
@@ -5,41 +5,58 @@ interface PlayerControlsProps {
 	player: React.RefObject<HTMLAudioElement> | null;
 }
 
+enum PlayerStatus {
+	PLAY = "play",
+	STOP = "stop",
+	PAUSE = "pause",
+}
 export const PlayerControls = (props: PlayerControlsProps) => {
 	const { player } = props;
+	const [status, setStatus] = React.useState<PlayerStatus>(PlayerStatus.STOP);
 
 	const playAduio = () => {
 		if (!player || !player.current) return;
+		setStatus(PlayerStatus.PLAY);
 		player.current.play();
 	};
 
 	const stopAduio = () => {
 		if (!player || !player.current) return;
+		setStatus(PlayerStatus.STOP);
 		player.current.pause();
 		player.current.currentTime = 0;
 	};
 
 	const pauseAduio = () => {
 		if (!player) return;
+		setStatus(PlayerStatus.PAUSE);
 		player.current?.pause();
 	};
+
+	const buttonPlay: React.ReactElement = (
+		<button
+			className="button_control play"
+			onClick={playAduio}
+		></button>
+	);
+
+	const buttonStop: React.ReactElement = (
+		<button
+			className="button_control stop"
+			onClick={stopAduio}
+		></button>
+	);
+
+	const buttonPause: React.ReactElement = (
+		<button
+			className="button_control pause"
+			onClick={pauseAduio}
+		></button>
+	);
 	return (
 		<div className="player_control">
-			<button
-				className="control_button play"
-				onClick={playAduio}
-			></button>
-			<button
-				className="control_button stop"
-				onClick={stopAduio}
-			></button>
-
-			<button
-				className="control_button pause"
-				onClick={pauseAduio}
-			></button>
-
-			
+			{status === PlayerStatus.PLAY ? buttonPause : buttonPlay}
+			{buttonStop}
 		</div>
 	);
 };
